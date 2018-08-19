392ed8933684595fe778cc7db074092a8d06c29d
Alex Rusin <alexrusin_2000@yahoo.com>
2018-08-19 12:08:20 -0700

    Change New Route words

--- resources/views/hooks/create.blade.php
+++ resources/views/hooks/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Create Route</div>
+                <div class="panel-heading">New Route</div>
 
                 <div class="panel-body">
                    <form method="POST" action="{{route('hooks.store')}}">
